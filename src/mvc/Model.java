57de89688f154895b1c5760e934fb49487dbbac7
-Ace <143662232+minusAce@users.noreply.github.com>
2024-03-08 12:34:47 -0800

    Update Model.java

--- src/mvc/Model.java
+++ src/mvc/Model.java
@@ -1,20 +1,37 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package mvc;
 
 import java.io.Serializable;
 
-public class Model extends Publisher implements Serializable
-{
-    private Boolean unsavedChanges;
-    private String fileName;
-    public Model()
-    {
-        unsavedChanges = false;
-        fileName = null;
+public class Model extends Publisher implements Serializable {
+    private Boolean unsavedChanges = false;
+    private String fileName = null;
+
+    public Model() {
     }
 
-    public void changed()
-    {
-        unsavedChanges = true;
-        notifySubscribers();
+    public void changed() {
+        this.unsavedChanges = true;
+        this.notifySubscribers();
+    }
+
+    public boolean getUnsavedChanges() {
+        return this.unsavedChanges;
+    }
+
+    public String getFileName() {
+        return this.fileName;
+    }
+
+    public void setFileName(String fName) {
+        this.fileName = fName;
+    }
+
+    public void setUnsavedChanges(boolean b) {
+        this.unsavedChanges = b;
     }
 }
