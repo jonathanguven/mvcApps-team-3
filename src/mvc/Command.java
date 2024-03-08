b006f580ec33521c2d1f7812f75ed8d02233f232
Jonathan <jonathanguven@gmail.com>
2024-03-07 22:29:30 -0800

    implemented AppFactory and Command files in mvc package

--- src/mvc/Command.java
+++ src/mvc/Command.java
@@ -1,4 +1,9 @@
 package mvc;
 
-public class Command {
+abstract public class Command {
+    public Model model;
+    public abstract void execute() throws Exception;
+    public Command(Model m){
+        this.model = m;
+    }
 }
