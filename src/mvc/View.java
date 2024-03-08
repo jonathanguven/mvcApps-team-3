7561f5a4c7b0b72ecf42a041e73eb740c086879c
-Ace <143662232+minusAce@users.noreply.github.com>
2024-03-08 12:35:22 -0800

    Update View.java

--- src/mvc/View.java
+++ src/mvc/View.java
@@ -1,4 +1,28 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package mvc;
 
-public class View {
+import javax.swing.JPanel;
+
+public class View extends JPanel implements Subscriber {
+    public Model model;
+
+    public View(Model newModel) {
+        this.model = newModel;
+        this.model.subscribe(this);
+    }
+
+    public void update() {
+        this.repaint();
+    }
+
+    public void setModel(Model newModel) {
+        this.model.unsubscribe(this);
+        this.model = newModel;
+        this.model.subscribe(this);
+        this.repaint();
+    }
 }
