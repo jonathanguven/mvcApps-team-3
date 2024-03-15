e7b00cb275c8da27ad3eaa86b5fe7941164a3673
-Ace <143662232+minusAce@users.noreply.github.com>
2024-03-14 17:29:40 -0700

    Update GridPanel.java

--- src/CALab/GridPanel.java
+++ src/CALab/GridPanel.java
@@ -1,4 +1,41 @@
 package CALab;
 
-public class GridPanel {
+import mvc.AppFactory;
+import mvc.AppPanel;
+import stopLight.StoplightFactory;
+import stopLight.StoplightPanel;
+
+import javax.swing.*;
+
+public class GridPanel extends AppPanel
+{
+    private JButton run1;
+    private JButton run50;
+    private JButton clear;
+    private JButton populate;
+
+    public GridPanel(AppFactory factory)
+    {
+        super(factory);
+        run1 = new JButton("Run1");
+        run1.addActionListener(this);
+        run50 = new JButton("Run50");
+        run50.addActionListener(this);
+        clear = new JButton("Clear");
+        clear.addActionListener(this);
+        populate = new JButton("Populate");
+        populate.addActionListener(this);
+        
+        controls.add(run1);
+        controls.add(run50);
+        controls.add(clear);
+        controls.add(populate);
+    }
+
+    public static void main(String[] args)
+    {
+        AppFactory factory = new StoplightFactory();
+        AppPanel panel = new StoplightPanel(factory);
+        panel.display();
+    }
 }
