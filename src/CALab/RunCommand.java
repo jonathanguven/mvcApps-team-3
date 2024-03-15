e47e00594e63d509d1126f92a204f5dd163c9c18
-Ace <143662232+minusAce@users.noreply.github.com>
2024-03-14 17:30:07 -0700

    Update RunCommand.java

--- src/CALab/RunCommand.java
+++ src/CALab/RunCommand.java
@@ -1,4 +1,22 @@
 package CALab;
 
-public class RunCommand {
+import mvc.Command;
+import mvc.Model;
+
+public class RunCommand extends Command
+{
+    public RunCommand(Model m)
+    {
+        super(m);
+    }
+
+    @Override
+    public void execute()
+    {
+        Grid grid = (Grid) model;
+        // if run1
+        grid.updateLoop(1);
+        // if run 50
+        grid.updateLoop(50);
+    }
 }
