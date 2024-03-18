7e91b9146076159963b3245c600c753d1db0f14d
Jonathan <jonathanguven@gmail.com>
2024-03-17 23:15:52 -0700

    fixed reset method to make populate random

--- src/Life/Agent.java
+++ src/Life/Agent.java
@@ -14,7 +14,8 @@ public class Agent extends Cell {
     }
 
     @Override
-    public void interact() {}
+    public void interact() {
+    }
 
     @Override
     public void update() {
@@ -26,8 +27,7 @@ public class Agent extends Cell {
     public void nextState() {
         if (status == 0 && ambience == 3) {
             status = 1;
-        }
-        else if (status == 1 && (ambience <= 1 || ambience >= 4)) {
+        } else if (status == 1 && (ambience <= 1 || ambience >= 4)) {
             status = 0;
         }
     }
@@ -35,9 +35,8 @@ public class Agent extends Cell {
     @Override
     public void reset(boolean randomly) {
         if (randomly) {
-            status = 1;
-        }
-        else {
+            status = (int) (Math.random() * 2);
+        } else {
             status = 0;
         }
     }
@@ -51,8 +50,7 @@ public class Agent extends Cell {
     public Color getColor() {
         if (getStatus() == 0) {
             return Color.RED;
-        }
-        else {
+        } else {
             return Color.GREEN;
         }
     }
